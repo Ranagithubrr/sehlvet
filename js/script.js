76da652ccab5bf10b556e1845e8f80765207b67b
ranagithubrr <mr6430119@gmail.com>
2024-01-02 16:02:08 +0600

    Complete

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$ (function (){
+    $('.customerFeedback').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<span class="PrevArrow"><i class="fa fa-angle-left" aria-hidden="true"></i></span>',
+        nextArrow: '<span class="NextArrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span>',
+      });
+})
