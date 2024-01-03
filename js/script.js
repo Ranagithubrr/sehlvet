5dca9a67b6e6d89d023779561f00b5ea872a58c4
ranagithubrr <mr6430119@gmail.com>
2024-01-04 01:53:53 +0600

    Responsive done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
-$ (function (){
-    $('.customerFeedback').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow: '<span class="PrevArrow"><i class="fa fa-angle-left" aria-hidden="true"></i></span>',
-        nextArrow: '<span class="NextArrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span>',
-      });
+$(function () {
+  AOS.init();
+  $('.customerFeedback').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    prevArrow: '<span class="PrevArrow"><i class="fa fa-angle-left" aria-hidden="true"></i></span>',
+    nextArrow: '<span class="NextArrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span>',
+  });
+  const menu = document.querySelector('.menu');
+  document.querySelector('.hamburger-button').addEventListener('click', function () {
+    this.classList.toggle('open');
+    menu.classList.add('show');
+  });
+  document.querySelector('.closeIcon').addEventListener('click', function () {
+    menu.classList.remove('show');
+  });
+  const menuItems = document.querySelectorAll('.menuItems');
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+      // Run your function when any item is clicked
+      removeClassFunc();
+    });
+  });
+  function removeClassFunc() {
+    menu.classList.remove('show');
+  }
 })
